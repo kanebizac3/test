4f312b39439c472b77ab1c10e4bf9670125a7e26
shohei <kanesho1216@gmail.com>
2025-04-14 17:56:02 +0900

    gomimon ログイン画面等修正

--- gomimon/signals.py
+++ gomimon/signals.py
@@ -8,8 +8,11 @@ DEFAULT_REGISTRATION_POINTS = 1  # 初期ポイント
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance, points=DEFAULT_REGISTRATION_POINTS)
+        Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except Profile.DoesNotExist:
+        Profile.objects.create(user=instance)
