bf5c5a332245381a17568f3729a9bb77aa6bc62f
shohei <kanesho1216@gmail.com>
2025-05-10 21:02:36 +0900

    admin追加

--- poopgame/admin.py
+++ poopgame/admin.py
@@ -1,9 +1,12 @@
 # poopgame/admin.py
 from django.contrib import admin
 from django.contrib.auth.models import User
+from .models import UnpPoint
+
 
 # 標準の User モデルをそのまま使用（カスタムユーザーを使わない場合）
 admin.site.unregister(User)
+admin.site.register(UnpPoint)
 
 @admin.register(User)
 class CustomUserAdmin(admin.ModelAdmin):
